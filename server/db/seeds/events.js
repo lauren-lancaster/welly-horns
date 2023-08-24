05132952b1910e8828a8cfcbda5cb19eb1c7677c
Lauren Lancaster <737lanc@gmail.com>
2023-08-25 11:17:15 +1200

    update seeds

--- server/db/seeds/events.js
+++ server/db/seeds/events.js
@@ -8,11 +8,27 @@ exports.seed = async function (knex) {
   await knex('events').insert([
     {
       id: 1,
-      title: 'rowValue1',
+      title: 'Warm Up',
       description: 'warm up class for everyone',
       location: 'poneke art center',
-      date: 'tomorrow',
+      date: 'monday',
       time: '8am-10am',
+    },
+    {
+      id: 2,
+      title: 'Group Lesson',
+      description: "play what you've prepared",
+      location: 'poneke art center',
+      date: 'thursday',
+      time: '6pm-18pm',
+    },
+    {
+      id: 3,
+      title: 'Concert',
+      description: 'come watch some cool horns',
+      location: 'St Andrews',
+      date: 'Fridat',
+      time: '12am-1:30pm',
     },
   ])
 }
