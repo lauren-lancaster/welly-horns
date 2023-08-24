c29b90691821bd09d6b7e4574c88276bc87a8d07
Lauren Lancaster <737lanc@gmail.com>
2023-08-25 11:17:39 +1200

    add new components

--- client/router.tsx
+++ client/router.tsx
@@ -6,11 +6,17 @@ import {
 
 import App from './components/App'
 import Home from './components/Home'
+import About from './components/About'
+import Events from './components/Events'
+import Resources from './components/Resources'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="events" element={<Events />} />
+      <Route path="resources" element={<Resources />} />
     </Route>,
   ),
 )
